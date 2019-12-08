2909b8d0352c99b1aa1097a6a6e4a8d429f79655
sapeled3 <oyinkuromosesvictor@outlook.com>
2019-12-09 00:59:21 +0100

    make db public

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ dotenv.config();
 
 const mongo = process.env.MONGODB_URI;
 
-mongoose.connect(mongo, {
+mongoose.connect('mongodb+srv://moses:slugterra@cluster0-czitz.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
